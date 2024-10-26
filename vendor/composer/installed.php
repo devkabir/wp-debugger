82179e6cf6fa9306da3f1aaee119d18e6da98332
Kabir Khyrul <dev.kabir01@gmail.com>
2024-10-26 13:04:18 +0600

    vendor added

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'devkabir/wp-debugger',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'ca22310bd5b1131f1cf9d8c2652ad260762a2eb7',
+        'type' => 'wordpress-plugin',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'devkabir/wp-debugger' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'ca22310bd5b1131f1cf9d8c2652ad260762a2eb7',
+            'type' => 'wordpress-plugin',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
