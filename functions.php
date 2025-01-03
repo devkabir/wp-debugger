67175f76e4ed1c7a172cf02ce9b586b7d99eb848
Kabir Khyrul <dev.kabir01@gmail.com>
2025-01-03 19:26:56 +0600

    update

--- functions.php
+++ functions.php
@@ -3,16 +3,35 @@
 /**
  * Logs a message to a specified directory.
  *
- * @param mixed  $message The message to be logged.
+ * @param mixed $message The message to be logged.
  *
  * @return void
  * @throws \Exception
  */
-function write_log(...$messages)
-{
-	foreach ($messages as $message) {
-		DevKabir\WPDebugger\Plugin::get_instance()->log($message);
+function write_log( ...$messages ) {
+	foreach ( $messages as $message ) {
+		DevKabir\WPDebugger\Plugin::get_instance()->log( $message );
 	}
+}
+
+/**
+ * Log stack trace.
+ * 
+ * @param array $trace The stack trace.
+ *
+ * @return void
+ */
+function log_stack_trace( $trace ) {
+	$formatted_trace = array();
+	foreach ( $trace as $value ) {
+		$formatted_trace[ $value['file'] . ':' . $value['line'] ] = array(
+			'function' => $value['function'],
+			'args'     => isset( $value['args'] ) ? array_filter( $value['args'], 'is_string' ) : false,
+		);
+	}
+
+	$formatted_trace = array_reverse( $formatted_trace );
+	write_log( $formatted_trace );
 }
 
 /**
@@ -21,8 +40,7 @@ function write_log(...$messages)
  * @return void
  * @throws Exception
  */
-function init_debugger()
-{
+function init_debugger() {
 	DevKabir\WPDebugger\Plugin::get_instance()->throw_exception();
 }
 
@@ -34,10 +52,9 @@ function init_debugger()
  * @return void
  * @throws Exception
  */
-function dump($variable)
-{
-	$compiled_data = DevKabir\WPDebugger\Template::compile(array('{{content}}' => var_export($variable, true)), DevKabir\WPDebugger\Template::get_part('dump'));
-	echo DevKabir\WPDebugger\Template::compile(array('{{content}}' => $compiled_data), DevKabir\WPDebugger\Template::get_layout());
+function dump( $variable ) {
+	$compiled_data = DevKabir\WPDebugger\Template::compile( array( '{{content}}' => var_export( $variable, true ) ), DevKabir\WPDebugger\Template::get_part( 'dump' ) );
+	echo DevKabir\WPDebugger\Template::compile( array( '{{content}}' => $compiled_data ), DevKabir\WPDebugger\Template::get_layout() );
 }
 
 /**
@@ -48,9 +65,8 @@ function dump($variable)
  * @return void
  * @throws Exception
  */
-function dd(...$data)
-{
-	dump(func_get_args());
+function dd( ...$data ) {
+	dump( func_get_args() );
 	die;
 }
 
@@ -61,10 +77,9 @@ function dd(...$data)
  *
  * @var string $id The ID of the option to be saved in the database.
  */
-function render_settings_fields(array $args)
-{
-	$option = get_option($args['id']);
-	echo '<input type="checkbox" id="' . $args['id'] . '" name="' . $args['id'] . '" value="1"' . checked(1, $option, false) . ' />';
+function render_settings_fields( array $args ) {
+	$option = get_option( $args['id'] );
+	echo '<input type="checkbox" id="' . $args['id'] . '" name="' . $args['id'] . '" value="1"' . checked( 1, $option, false ) . ' />';
 	echo '<label for="' . $args['id'] . '"> ' . $args['label'] . '</label>';
 }
 
@@ -73,7 +88,6 @@ function render_settings_fields(array $args)
  *
  * @return bool Whether the debug bar should be shown.
  */
-function show_debugbar(): bool
-{
-	return (bool) get_option('show_debugbar', false);
+function show_debugbar(): bool {
+	return (bool) get_option( 'show_debugbar', false );
 }
